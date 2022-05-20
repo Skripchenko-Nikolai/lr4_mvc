5897c80496d6cf8c1e97ce2fa98e1ffe58e9ccbc
Skripchenko-Nikolai <81862624+Skripchenko-Nikolai@users.noreply.github.com>
2022-05-20 15:46:30 +0600

    Добавьте файлы проекта.

---
+++ lr4/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab4.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public async Task<string> GetData()
+    {
+        var httpClientHandler = new HttpClientHandler();
+        httpClientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+        var httpClient = new HttpClient(httpClientHandler);
+        httpClient.BaseAddress = new Uri("https://localhost:3336/api/city");
+        httpClient.DefaultRequestHeaders.Accept.Clear();
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        var response = await httpClient.GetAsync("/api/city");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
